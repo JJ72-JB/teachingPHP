2d093155e6a3bab10d45768e9e77662287768dd1
Juan Bajonero <juan.bajonero@outlook.com>
2020-05-25 15:41:03 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css"/>
+
+    <title>Learning php</title>
+</head>
+<body>
+    <center>
+        <p class="description">Ingresa tu nombre:</p>
+        <input type="text" placeholder="Escribe tu nombre"/>
+        <button type="submit" class="btn">Guardar</button>
+    </center>
+</body>
+</html>
